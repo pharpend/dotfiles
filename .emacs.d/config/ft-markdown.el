b97c8d0273382c3d9ad0f008ab3e494e7d4a2428
Peter Harpending <pharpend2@gmail.com>
2014-03-31 08:18:59 -0600

    dotfiles-valentine commit for Mon Mar 31 08:18:59 MDT 2014

--- .emacs.d/config/ft-markdown.el
+++ .emacs.d/config/ft-markdown.el
@@ -1,11 +1,13 @@
 ;; Filetype options for Markdown
 
 (setq-local indent-tabs-mode nil)
-(setq-local tab-width 2)
-(setq-local tab-stop-list (number-sequence 2 200 2))
-(setq-local evil-shift-width 2)
+(setq-local tab-width 4)
+(setq-local tab-stop-list (number-sequence 4 400 4))
+(setq-local evil-shift-width 4)
 
-(auto-fill-mode 1)
+(smartparens-mode 1)
+(rainbow-mode 1)
+(rainbow-delimiters-mode 1)
 (flyspell-mode 1)
 (turn-on-pandoc)
 
