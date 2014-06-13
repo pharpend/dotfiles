f8f6704860fc66f36005cc2cc4be15cece85bc79
Peter Harpending <pharpend2@gmail.com>
2014-06-13 10:03:00 -0600

    dotfiles-valentine commit for Fri Jun 13 10:03:00 MDT 2014

---
+++ bin/spook.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+spook_words = File::read(File::dirname(File::expand_path(__FILE__)) +
+                         "/libspook.lines").split("\u0000\n")
+spook_n_words = (1..9).map do |x|
+  spook_words[Random::rand(spook_words.length)]
+end
+spook_n_s = spook_n_words.join(" ") + "\n"
+
+puts "Peter Harpending\n\n#{spook_n_s}"
