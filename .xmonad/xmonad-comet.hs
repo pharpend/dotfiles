d963f0bd1b8f6051b1a83f6c5a0b2af30ac89917
Peter Harpending <pharpend2@gmail.com>
2014-07-01 20:18:35 -0600

    dotfiles-comet commit for Tue Jul  1 20:18:35 MDT 2014

--- .xmonad/xmonad-comet.hs
+++ .xmonad/xmonad-comet.hs
@@ -92,8 +92,8 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
   , (( modm               , xK_w     ), spawn "dmenu_run"            )
   , (( modm               , xK_m     ), spawn "emacs"                )
   , (( modm               , xK_grave ), spawn "dwb"                  )
-  , (( modm               , xK_c     ), spawn "chromium"             )
-  , (( modm .|. shiftMask , xK_c     ), spawn "chromium --incognito" )
+  , (( modm               , xK_f     ), spawn "pcmanfm --no-desktop" )
+  , (( modm               , xK_c     ), spawn "chromium --incognito" )
   ] ++
 
   -- mod-N       - go to workspace N
