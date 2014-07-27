9a5b51296c7c2f18257b22575b571ad68a122dfe
Peter Harpending <pharpend2@gmail.com>
2014-07-27 03:51:36 -0600

    dotfiles-comet commit for Sun Jul 27 03:51:36 MDT 2014

--- .xmonad/xmonad-comet.hs
+++ .xmonad/xmonad-comet.hs
@@ -19,7 +19,7 @@ import qualified XMonad.StackSet as W
 
 main = xmonad myConf
 
-myConf = defaultConfig { terminal           = "terminator" 
+myConf = defaultConfig { terminal           = "sakura" 
                        , modMask            = mod4Mask
                        , borderWidth        = 1
                        , focusFollowsMouse  = False
