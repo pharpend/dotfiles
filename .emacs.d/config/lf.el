98f5d51fe577858469a3f788b5a73760b3fa3382
Peter Harpending <pharpend2@gmail.com>
2014-09-06 15:59:01 -0600

    dotfiles-valentine commit for Sat Sep  6 15:59:01 MDT 2014

--- .emacs.d/config/lf.el
+++ .emacs.d/config/lf.el
@@ -1,6 +1,6 @@
 ;; Configure the look and feel of Emacs
 (cond ((eq window-system 'x)
-       (set-frame-font "Source Code Pro 10")
+       (set-frame-font "Envy Code R 11")
        (load-theme 'monokai t)))
 
 (scroll-bar-mode -1)
