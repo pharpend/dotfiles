884081ebd6b1995e98cc1a672c6102a068f0fcd1
Peter Harpending <pharpend2@gmail.com>
2014-10-09 21:28:49 -0600

    dotfiles/valentine commit for Thu Oct  9 21:28:49 MDT 2014

---
+++ .emacs.d/config/ft-ml.el
@@ -0,0 +1,10 @@
+(highlight-indentation-current-column-mode 1)
+(rainbow-mode 1)
+(rainbow-delimiters-mode 1)
+(smartparens-mode 1)
+
+(setq-local indent-tabs-mode nil)
+(setq-local indent-line-function 'indent-relative-maybe)
+(setq-local tab-width 2)
+(setq-local tab-stop-list (number-sequence 2 200 2))
+(setq-local evil-shift-width 2)
