45074e512b86a648c69d228e91235bdb013200a4
Peter Harpending <pharpend2@gmail.com>
2014-10-18 15:54:11 -0500

    dotfiles/comet commit for Sat Oct 18 15:54:11 CDT 2014

--- .emacs.d/config/lf.el
+++ .emacs.d/config/lf.el
@@ -1,7 +1,7 @@
 ;; Configure the look and feel of Emacs
 (cond ((eq window-system 'x)
        (set-frame-font "Inconsolata-dz for Powerline 9")
-       (load-theme 'sanityinc-tomorrow-day t)))
+       (load-theme 'mccarthy t)))
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
