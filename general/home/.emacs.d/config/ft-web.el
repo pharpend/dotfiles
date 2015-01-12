d2e84767191c8764a04dee7d11bac83d2fdd0766
Peter Harpending <peter@harpending.org>
2015-01-12 05:45:17 -0700

    Edit ft-web and add git wrapper.

--- general/home/.emacs.d/config/ft-web.el
+++ general/home/.emacs.d/config/ft-web.el
@@ -1,10 +1,12 @@
-(setq indent-tabs-mode nil)
-(setq tab-stop-list (number-sequence 2 200 2))
-(setq tab-width 2)
-(setq evil-shift-width 2)
+(setq-local indent-tabs-mode nil)
+(setq-local tab-stop-list (number-sequence 2 200 2))
+(setq-local tab-width 2)
+(setq-local evil-shift-width 2)
+(setq-local web-mode-markup-indent-offset 2)
 
-(auto-complete-mode)
-(autopair-mode 1)
+(smartparens-mode 1)
 (rainbow-mode 1)
 (rainbow-delimiters-mode 1)
 (highlight-indentation-current-column-mode 1)
+
+(local-set-key (kbd "C-j") 'newline-and-indent)
