6041ee116b601575dcf2475f0fafdcae9b30531a
Peter Harpending <peter@harpending.org>
2015-10-11 06:43:13 -0600

    dotfiles-valentine commit for Sun Oct 11 06:43:13 MDT 2015

--- .emacs.d/config/lf.el
+++ .emacs.d/config/lf.el
@@ -6,8 +6,8 @@
   (cond
    ((string= system-name "valentine")
     ;; (set-frame-font "Fira Mono 30"))
-    (set-frame-font "Fira Mono 12"))
-    ;; (set-frame-font "Inconsolata-dz for Powerline 14"))
+    ;; (set-frame-font "Fira Mono 12"))
+    (set-frame-font "Inconsolata-dz for Powerline 12"))
    ((string= system-name "locust")
     (set-frame-font "Source Code Pro 9")))))
 
